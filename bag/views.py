102b28869ba447b4a16c1ee6b78302dcbd8bd30b
ofemiashiru <>
2023-08-29 22:48:00 +0000

    feat: start add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def see_shopping_bag(request):
     """ view returns the shopping bag page"""
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, product_id):
+    pass
