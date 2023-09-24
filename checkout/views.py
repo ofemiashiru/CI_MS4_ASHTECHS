9d6f0abf75dbc9b5150213f75505e82f0f75a850
ofemiashiru <>
2023-09-24 19:50:12 +0000

    refactor: update save info code

--- checkout/views.py
+++ checkout/views.py
@@ -29,7 +29,7 @@ def cache_checkout_data(request):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': dict(request.POST)['save_info'][0],
+            'save_info': request.POST.get('save_info'),
             'username': request.user
         })
         return HttpResponse(status=200)
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -29,4 +29,3 @@ class StripeWebhookHandler:
             content=f'Webhook was received: {event["type"]}',
             status=200
         )
-
