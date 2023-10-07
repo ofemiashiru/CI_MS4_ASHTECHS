dc0c696d3e367aace3bf934461e7d485ba10c47c
ofemiashiru <>
2023-10-07 17:29:11 +0000

    refactor: remove ratings field

--- products/models.py
+++ products/models.py
@@ -40,8 +40,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     new_arrival = models.BooleanField(null=False, blank=False, default=False)
