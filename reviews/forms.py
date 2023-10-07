f85dd1a12263c423158a61d1faefdc1179dfb82c
ofemiashiru <>
2023-10-07 08:14:28 +0000

    feat: further develop add reviews

---
+++ reviews/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from reviews.models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    review_content = forms.CharField(label="Content:",widget=forms.Textarea)
+    class Meta:
+        model = Review
+        fields = (
+            'title', 'review_content', 'rating',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'review_content': 'Write your review',
+            'rating': 'Rating out of 100',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- reviews/views.py
+++ reviews/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse,
+    redirect
+)
 from django.contrib import messages
 
 from products.models import Product
+from profiles.models import UserProfile
+from reviews.models import Review
+
+from reviews.forms import ReviewForm
 
 # Create your views here.
 
 
 def add_review(request, product_id):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST, instance=profile)
+
+        if review_form.is_valid():
+            review_form.save()
+
+            messages.success(request, 'Review succesfully added')
+            return HttpResponse('Posted')
+                
 
     if request.user.is_authenticated:
-        product = get_object_or_404(Product, id=product_id)
+
+        review_form = ReviewForm()
 
         context = {
-            'product': product
+            'product': product,
+            'review_form': review_form,
+            'from_review': True
         }
         return render(request, 'reviews/add_review.html', context)
 
