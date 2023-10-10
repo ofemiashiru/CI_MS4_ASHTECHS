7c5b622d7686a30b1a7ec33a5c479ee8995b5adf
ofemiashiru <>
2023-10-10 19:26:18 +0000

    add heroku app to allowed hosts

--- ash_techs/settings.py
+++ ash_techs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'ash-techs.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'ash-techs-a3f0a77bec88.herokuapp.com']
 
 
 # Application definition
