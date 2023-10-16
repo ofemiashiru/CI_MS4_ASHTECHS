e1d6823369882fb8bbcb95fcc4dc877b3d8a1fcd
ofemiashiru <>
2023-10-16 16:00:01 +0000

    feat: add images to home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from products.models import Product
+
+import random
 
 
 def index(request):
     """ view returns the index (home) page"""
-    return render(request, 'home/index.html')
+
+    products = Product.objects.all()
+    number_of_products = len(products)
+    product_one = products[random.randrange(number_of_products)]
+    product_two = products[random.randrange(number_of_products)]
+
+    context = {
+        'product_one': product_one,
+        'product_two': product_two
+    }
+
+    return render(request, 'home/index.html', context)
