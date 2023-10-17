a4dddd07261a3b26d1c016f4f487027879c00a85
ofemiashiru <>
2023-10-17 17:33:49 +0000

    refactor: make view pep8 compliant

--- home/views.py
+++ home/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from products.models import Product
 from django.db.models import Q
 
-import random
-
 
 def index(request):
-    """ view returns the index (home) page"""
-    product_one = Product.objects.filter(Q(clearance=True) & Q(is_accessory=False)).first()
-    product_two = Product.objects.filter(Q(deal=True) & Q(is_accessory=True)).last()
+    """ view returns the index (home) page with two images """
+    product_one = Product.objects.filter(
+        Q(clearance=True) & Q(is_accessory=False)).first()
+    product_two = Product.objects.filter(
+        Q(deal=True) & Q(is_accessory=True)).last()
 
     context = {
         'product_one': product_one,
