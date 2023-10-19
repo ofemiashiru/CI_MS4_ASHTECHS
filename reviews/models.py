1dc7cd324cbc8b2082e6f96158a36cd32012a5e8
ofemiashiru <>
2023-10-19 10:11:55 +0000

    refactor: make changes to review model

--- reviews/models.py
+++ reviews/models.py
@@ -6,8 +6,8 @@ from profiles.models import UserProfile
 
 class Review(models.Model):
     user_profile = models.ForeignKey(
-        UserProfile, on_delete=models.SET_NULL, null=True, blank=True,
-        related_name='reviews'
+        UserProfile, on_delete=models.CASCADE, null=False,
+        blank=False, default='', related_name='reviews'
     )
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, null=False, blank=False
@@ -15,7 +15,7 @@ class Review(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
     review_content = models.CharField(max_length=255, null=False, blank=False)
     rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+        max_digits=6, decimal_places=2, null=False, blank=False, default=0)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
